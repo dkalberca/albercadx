804c130f7b8f5edaba8cb784ec8c461d0d74dd03
dkalberca <53499684+dkalberca@users.noreply.github.com>
2019-07-31 09:27:53 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <html>
 <head><title>Hi!</title></head>
-  <h1>Hello World</h1>
+  <center><h1>Hello World</h1></center>
   <body>
-    <p>Hello World</p>
+    <center><p>Hello World</p></center>
   </body>
 </html>
