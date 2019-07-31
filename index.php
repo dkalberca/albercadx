1d6db1eb4b05497bd2d9ed413d5253201d39ce7b
dkalberca <53499684+dkalberca@users.noreply.github.com>
2019-07-31 09:32:15 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,17 @@
 <html>
 <head><title>Hi!</title></head>
+  <style>
+body {
+  background-color: red;
+}
+
+h1 {
+  color: white;
+  text-align: center;
+}
+p {
+  color:white;
+    }
   <center><h1>Hello World</h1></center>
   <body>
     <center><p>Hello World</p></center>
